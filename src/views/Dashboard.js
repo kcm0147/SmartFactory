d12d4763afa8212e0faa7a5ebba9e0952fae24bc
nowwater <nowwater0225@gmail.com>
2020-10-15 12:54:32 +0900

    First uploading this page

---
+++ src/views/Dashboard.js
@@ -0,0 +1,238 @@
+/*!
+
+=========================================================
+* Black Dashboard React v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// react plugin used to create charts
+import { Line, Bar } from "react-chartjs-2";
+
+// reactstrap components
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  //DropdownToggle,
+  //DropdownMenu,
+  //DropdownItem,
+  //UncontrolledDropdown,
+  //Label,
+  //FormGroup,
+  //Input,
+  //Table,
+  Row,
+  Col,
+  //UncontrolledTooltip
+} from "reactstrap";
+
+// core components
+import {
+  chartExample1,
+  chartExample2,
+  chartExample3,
+  chartExample4
+} from "variables/charts.js";
+
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bigChartData: "data1"
+    };
+  }
+  setBgChartData = name => {
+    this.setState({
+      bigChartData: name
+    });
+  };
+  render() { // Dashboard row 시작되는 위치
+    return (
+      <>
+        <div className="content">
+          <Row>
+            <Col xs="12">
+              <Card className="card-chart">
+                <CardHeader>
+                  <Row>
+                    <Col className="text-left" sm="6">
+                      <h5 className="card-category">효동이네 공장</h5>
+                      <CardTitle tag="h2">온-습도</CardTitle>
+                    </Col>
+                    <Col sm="6">
+                      <ButtonGroup
+                        className="btn-group-toggle float-right"
+                        data-toggle="buttons"
+                      >
+                        <Button
+                          tag="label"
+                          className={classNames("btn-simple", {
+                            active: this.state.bigChartData === "data1"
+                          })}
+                          color="info"
+                          id="0"
+                          size="sm"
+                          onClick={() => this.setBgChartData("data1")}
+                        >
+                          <input
+                            defaultChecked
+                            className="d-none"
+                            name="options"
+                            type="radio"
+                          />
+                          <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                            온-습도
+                          </span>
+                          <span className="d-block d-sm-none">
+                            <i className="tim-icons icon-single-02" />
+                          </span>
+                        </Button>
+                        <Button
+                          color="info"
+                          id="1"
+                          size="sm"
+                          tag="label"
+                          className={classNames("btn-simple", {
+                            active: this.state.bigChartData === "data2"
+                          })}
+                          onClick={() => this.setBgChartData("data2")}
+                        >
+                          <input
+                            className="d-none"
+                            name="options"
+                            type="radio"
+                          />
+                          <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                            온도
+                          </span>
+                          <span className="d-block d-sm-none">
+                            <i className="tim-icons icon-gift-2" />
+                          </span>
+                        </Button>
+                        <Button
+                          color="info"
+                          id="2"
+                          size="sm"
+                          tag="label"
+                          className={classNames("btn-simple", {
+                            active: this.state.bigChartData === "data3"
+                          })}
+                          onClick={() => this.setBgChartData("data3")}
+                        >
+                          <input
+                            className="d-none"
+                            name="options"
+                            type="radio"
+                          />
+                          <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                            습도
+                          </span>
+                          <span className="d-block d-sm-none">
+                            <i className="tim-icons icon-tap-02" />
+                          </span>
+                        </Button>
+                      </ButtonGroup>
+                    </Col>
+                  </Row>
+                </CardHeader>
+                <CardBody>
+                  <div className="chart-area">
+                    <Line
+                      data={chartExample1[this.state.bigChartData]}
+                      options={chartExample1.options}
+                    />
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+          <Col xs="12">
+              <Card className="card-chart">
+                <CardHeader>
+                  <h5 className="card-category">공정라인 1</h5>
+                  <CardTitle tag="h3">
+                    <i className="tim-icons icon-bell-55 text-info" />{" "}
+                    무게
+                  </CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <div className="chart-area">
+                    <Line
+                      data={chartExample2.data}
+                      options={chartExample2.options}
+                    />
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs="12">
+            <Card className="card-chart">
+                <CardHeader>
+                  <h5 className="card-category">공정라인 2</h5>
+                  <CardTitle tag="h3">
+                    <i className="tim-icons icon-delivery-fast text-primary" />{" "}
+                    전압
+                  </CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <div className="chart-area">
+                    <Bar
+                      data={chartExample3.data}
+                      options={chartExample3.options}
+                    />
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs="12">
+            <Card className="card-chart">
+                <CardHeader>
+                  <h5 className="card-category">공정라인 3</h5>
+                  <CardTitle tag="h3">
+                    <i className="tim-icons icon-send text-success" /> 전류
+                  </CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <div className="chart-area">
+                    <Line
+                      data={chartExample4.data}
+                      options={chartExample4.options}
+                    />
+                  </div>
+                </CardBody>
+              </Card>       
+            </Col>
+          </Row>
+          {/* 나머지 차트 3개 */}
+          {/* TASK, Today */}
+          
+        </div>
+      </>
+    );
+  }
+}
+
+export default Dashboard;
