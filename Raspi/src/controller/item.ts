43a1d4f4f5505ee24f8e9b8f4cd09f41f7ed93ca
kcm0147 <kcm0147@hanmail.net>
2020-11-01 22:20:15 +0900

    add Raspi

---
+++ Raspi/src/controller/item.ts
@@ -0,0 +1,50 @@
+import { gql } from "apollo-boost";
+
+export interface Temperature {
+  id : string,
+  name : string,
+  temperature : string
+}
+export interface Humidity{
+  id : string,
+  name : string,
+  humidity : string
+}
+
+
+// export const GET_ITEMS = gql`
+// query {
+//   items {
+//     id, name
+//   }
+// }
+// `;
+
+// export const GET_ITEM = gql`
+// query ($id : Int!){
+//   item(id : $id) {
+//     id 
+//     name
+//     temperature
+//     humidity
+//   }
+// }
+// `;
+
+export const ADD_TEMPERATURE = gql`
+mutation ($id : String!, $name : String!, $temperature : String!){
+  result : addTemperature(id:$id,name : $name, temperature : $temperature) 
+}
+`;
+
+export const ADD_HUMIDITY = gql`
+mutation ($id: String!,$name : String!,$humidity : String!){
+  result : addHumidity(id:$id,name : $name,humidity : $humidity) 
+}
+`;
+
+// export const DEL_ITEM = gql`
+// mutation itemData($id : Int!){
+//   result : delItem(id : $id)
+// }
+// `;
