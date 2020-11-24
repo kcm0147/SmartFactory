95e3c6552b9591f50ca355bb291e5c1a5d641f29
nowwater <nowwater0225@gmail.com>
2020-11-24 23:03:50 +0900

    Updated client page by adding graph monitoring weight sensor

--- Client/src/views/Dashboard.js
+++ Client/src/views/Dashboard.js
@@ -1,87 +1,185 @@
-/*!
-
-=========================================================
-* Black Dashboard React v1.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/black-dashboard-react
-* Copyright 2020 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/black-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
-import { Route, Switch, Link } from 'react-router-dom';
-import Processline from "./Processline"
+// nodejs library that concatenates classes
+// import classNames from "classnames";
+// react plugin used to create charts
+import { Bar } from "react-chartjs-2";
 
 // reactstrap components
-import { Card, CardHeader, CardBody, Row, Col } from "reactstrap";
+import {
+  // Button,
+  // ButtonGroup,
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  //DropdownToggle,
+  //DropdownMenu,
+  //DropdownItem,
+  //UncontrolledDropdown,
+  //Label,
+  //FormGroup,
+  //Input,
+  //Table,
+  Row,
+  Col,
+  //UncontrolledTooltip
+} from "reactstrap";
+
+// core components
+import {
+  // chartExample1,
+  // chartExample2,
+  chartExample3,
+  // chartExample4
+} from "variables/charts.js";
+
+import TemperatureChart from "variables/TemperatureChart.js"
+import HumidityChart from "variables/HumidityChart.js"
+import WeightChart from "variables/WeightChart.js"
 
 class Dashboard extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bigChartData: "data1"
+    };
+  }
+
+  setBgChartData = name => {
+    this.setState({
+      bigChartData: name
+    });
+  };
+  render() { // Dashboard row 시작되는 위치
     return (
-      <div className="content">
-        <Switch>
-          <Route path="/admin/dashboard/processline" component={Processline} />
-        </Switch>
-        <h2> Dashboard </h2>
-        <Row>
-            <Col md="12">
+      <>
+        <div className="content">
+          <Row>
+          <Col xs="6">
               <Card>
                 <CardHeader>
-                  <h4 className="title">공정라인 리스트</h4>
-                  <p className="category">
-                    모니터링할 공정라인을 선택하세요.
-                  </p>
-                </CardHeader>
-                <CardBody className="all-icons">
                   <Row>
-                    <Col
-                      className="font-icon-list col-xs-6 col-xs-6"
-                      lg="4"
-                      md="3"
-                      sm="4"
-                    >
-                      <Link to="/admin/dashboard/processline">
-                        <button style={{background:"#11ffee00",width:"100%"}} className="font-icon-detail">
-                          <i className="tim-icons icon-alert-circle-exc" />
-                          <p style={{fontSize:16,fontWeight:"bold"}}>공정라인 1</p>
-                        </button>
-                      </Link>
-                    </Col>
-                    <Col
-                      className="font-icon-list col-xs-6 col-xs-6"
-                      lg="4"
-                      md="3"
-                      sm="4"
-                    >
-                      <button style={{background:"#11ffee00",width:"100%"}} className="font-icon-detail">
-                        <i className="tim-icons icon-alert-circle-exc" />
-                        <p style={{fontSize:16,fontWeight:"bold"}}>공정라인 2</p>
-                      </button>
-                    </Col>
-                    <Col
-                      className="font-icon-list col-xs-6 col-xs-6"
-                      lg="4"
-                      md="3"
-                      sm="4"
-                    >
-                      <button style={{background:"#11ffee00",width:"100%"}} className="font-icon-detail">
-                        <i className="tim-icons icon-alert-circle-exc" />
-                        <p style={{fontSize:16,fontWeight:"bold"}}>공정라인 3</p>
-                      </button>
+                    <Col className="text-left" sm="6">
+                    <h5 className="card-category">Process Line 1</h5>
+                      <CardTitle tag="h3">
+                        <i className="tim-icons icon-single-02" />
+                        Temperature
+                      </CardTitle>
                     </Col>
                   </Row>
+                </CardHeader>
+                <CardBody>
+                  <div className="chart-area">
+                    <TemperatureChart/>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col xs="6">
+              <Card>
+                <CardHeader>
+                  <Row>
+                    <Col className="text-left" sm="6">
+                    <h5 className="card-category">Process Line 1</h5>
+                      <CardTitle tag="h3">
+                      <i className="tim-icons icon-single-02" />
+                        Fire detection
+                      </CardTitle>
+                    </Col>
+                  </Row>
+                </CardHeader>
+                <CardBody>
+                  <div className="chart-area">
+                    <TemperatureChart/>
+                  </div>
                 </CardBody>
               </Card>
             </Col>
           </Row>
-      </div>
+          <Row>
+            <Col xs="6">
+              <Card className="card-chart">
+                <CardHeader>
+                  <Row>
+                    <Col className="text-left" sm="6">
+                    <h5 className="card-category">Process Line 2</h5>
+                      <CardTitle tag="h3">
+                      <i className="tim-icons icon-gift-2" />
+                        CCTV
+                      </CardTitle>
+                    </Col>
+                  </Row>
+                </CardHeader>
+                <CardBody>
+                  <div>
+                    <HumidityChart/>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col xs="6">
+              <Card className="card-chart">
+                <CardHeader>
+                  <Row>
+                    <Col className="text-left" sm="6">
+                    <h5 className="card-category"> Process Line 2</h5>
+                      <CardTitle tag="h3">
+                        <i className="tim-icons icon-gift-2" />
+                        Infrared temperature
+                      </CardTitle>
+                    </Col>
+                  </Row>
+                </CardHeader>
+                <CardBody>
+                  <div>
+                    <HumidityChart/>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs="6">
+              <Card className="card-chart">
+                <CardHeader>
+                  <h5 className="card-category">Process Line 3</h5>
+                  <CardTitle tag="h3">
+                    <i className="tim-icons icon-delivery-fast text-primary" />{" "}
+                    Temperature
+                  </CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <div>
+                    <WeightChart/>
+                  </div>
+                  {/* <div className="chart-area">
+                    <Bar
+                      data={chartExample3.data}
+                      options={chartExample3.options}
+                    />
+                  </div> */}
+                </CardBody>
+              </Card>
+            </Col>
+            <Col xs="6">
+              <Card className="card-chart">
+                <CardHeader>
+                  <h5 className="card-category">Process Line 3</h5>
+                  <CardTitle tag="h3">
+                    <i className="tim-icons icon-delivery-fast text-primary" />{" "}
+                    Weight
+                  </CardTitle>
+                </CardHeader>
+                <CardBody>
+                  <div>
+                    <WeightChart/>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </>
     );
   }
 }
