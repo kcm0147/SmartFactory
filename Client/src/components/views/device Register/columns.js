bbd8b16200728b4c2f31c7888ce2fba4ed7a7b51
Hruthvik <34919558+Hruthvik@users.noreply.github.com>
2020-12-01 08:50:54 +0900

    Add files via upload

---
+++ Client/src/components/views/device Register/columns.js
@@ -0,0 +1,20 @@
+export const COLUMNS = [
+
+    {
+        Header : 'Device Name',
+        accessor : 'device_name'
+    },
+    {
+        Header : "Device ID",
+        accessor : 'device_id'
+    },
+    {
+        Header : "Measured value type",
+        accessor : 'measured_value_type'
+    },
+    {
+        Header : "Graph Location",
+        accessor : 'graph_location'
+    }
+
+]
