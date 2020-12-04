5d5fd9221023f041d62265efaa4f82cb827fa606
o-star <uh04049@naver.com>
2020-12-04 17:06:44 +0900

    dynamic3

--- Client/src/variables/TemperatureChart.js
+++ Client/src/variables/TemperatureChart.js
@@ -8,6 +8,8 @@ import gql from "graphql-tag";
 class TemperatureChart extends React.Component {
   constructor(props) {
     super(props);
+    this.mine = true;
+    this.mytemp = { temperatures: new Array() };
     this.querystr = gql`query {
       temperatures {
         label: id
@@ -30,22 +32,32 @@ class TemperatureChart extends React.Component {
       <Subscription subscription={gql`${this.subscribestr}`}>
         {({ data, loading }) => {
           if (loading) return null;
+          if (data.newTemperature.label != this.props.line) this.mine = false;
+          else this.mine = true;
           const newdata = data;
 
           return <Query query={gql`${this.querystr}`}>
             {({ data, loading }) => {
               if (loading) return null;
 
-              data.temperatures.push(newdata.newTemperature);
-              while(data.temperatures.length >20) data.temperatures.shift();
+              if (this.mine) {
+                data.temperatures.push(newdata.newTemperature);
+                this.mytemp.temperatures = [];
+                let length = data.temperatures.length;
+                for (let i = 0; i < length; i++)
+                  if (data.temperatures[i].label == this.props.line) this.mytemp.temperatures.push(data.temperatures[i]);
+                while (data.temperatures.length > 40) data.temperatures.shift();
+                // console.log(data);
+                // console.log(this.mytemp);
+              }
 
               // create graphql2chartjs instance
-              let g2c = new graphql2chartjs(data, () => {
+              let g2c = new graphql2chartjs(this.mytemp, () => {
                 return {
                   chartType: 'line',
                   pointBackgroundColor: 'yellow',
                   borderColor: 'yellow',
-                  backgroundColor:"rgba(29,140,248,0.1)",
+                  backgroundColor: "rgba(29,140,248,0.1)",
                   pointHoverBackgroundColor: 'red',
                   borderWidth: 1
                 };
@@ -54,16 +66,16 @@ class TemperatureChart extends React.Component {
               // render chart with g2c data :)
               return (
                 <Line data={g2c.data}
-                options={{
-                  scales: {
-                    yAxes: [{
-                      ticks: {
-                        suggestedMin: 15,
-                        suggestedMax: 30
-                      }
-                    }]
-                  }
-                }}/>
+                  options={{
+                    scales: {
+                      yAxes: [{
+                        ticks: {
+                          suggestedMin: 15,
+                          suggestedMax: 30
+                        }
+                      }]
+                    }
+                  }} />
               );
             }}
           </Query>
